bc045e3de176a7e869ad5472c0fe538ee3266b3a
GoodGuyGregory <greg.witt625@gmail.com>
2024-12-19 18:34:41 -0500

    removes chroma db from the music_parser_service

--- music_parser_service.py
+++ music_parser_service.py
@@ -3,8 +3,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain_chroma import Chroma
 from langchain_community.document_loaders import WikipediaLoader
 from langchain.prompts import PromptTemplate
 import re
