6e8b0b81ea551353fe3986940f68d5c983feef6c
GoodGuyGregory <greg.witt625@gmail.com>
2024-12-29 22:40:53 -0500

    adds list 3 for th albums and bands

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def main():
 
 
     pprint.pprint(
-        qa_with_source("Tell me about Khruangbin releases in 2024.")
+        qa_with_source("Tell me about Helado Negro releases in 2024.")
     )
 
 main()
